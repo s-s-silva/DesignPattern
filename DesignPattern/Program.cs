37e7b4f43061b480e08c8182a9ede806e9e727ed
Stefanie Soares da Silva <s.soares.da.silva@avanade.com>
2024-01-16 12:45:26 -0300

    Commit Inicial

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using DesignPattern.Comportamental.Command;
+using DesignPattern.Facade;
+
+#region --- Command ---
+
+Console.WriteLine("\n --- Command --- \n");
+Luz luz = new Luz();
+Interruptor interruptor = new Interruptor();
+
+ICommand ligarCommand = new LigarCommand(luz);
+ICommand desligarCommand = new DesligarCommand(luz);
+
+interruptor.SetCommand(ligarCommand);
+interruptor.PressionarBotao();
+
+interruptor.SetCommand(desligarCommand);
+interruptor.PressionarBotao();
+
+#endregion
+
+#region --- Facade ---
+Console.WriteLine("\n --- Facade --- \n");
+ReservasDeViagemFacade reserva = new ReservasDeViagemFacade();
+reserva.ReservarHotel();
+reserva.ReservarVoo();
+reserva.FinalizarCompra();
+
+#endregion
